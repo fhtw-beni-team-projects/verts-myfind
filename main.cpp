bf31f8a23982568cd212812f4d741fed9c1cd59c
Benedikt Galbavy <if22b082@technikum-wien.at>
2023-09-18 17:21:39 +0200

    added comments

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello World!" << std::endl;
-
-	
+	std::cout << "Hello World!" << std::endl; // This code prints "Hello World!" to the console
 }
