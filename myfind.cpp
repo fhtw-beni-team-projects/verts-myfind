a3744f93b51ebde199a79025a2ae78263cf36a93
Benedikt Galbavy <if22b082@technikum-wien.at>
2023-09-21 14:56:47 +0200

    option & argument parsing

--- myfind.cpp
+++ myfind.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+#include <string>
+#include <unistd.h>
+#include <vector>
 
 int main(int argc, char* argv[]) {
 	std::cout << "Hello World!" << std::endl; // This code prints "Hello World!" to the console
+
+	while (true) {
+		switch (getopt(argc, argv, "iR")) {
+			case 'i':
+				std::cout << "Specified -i" << std::endl;
+				continue;
+			case 'R':
+				std::cout << "Specified -R" << std::endl;
+				continue;
+			case -1:
+				break;
+		}
+
+		break;
+	}
+
+	std::string searchpath;
+	std::vector<std::string> filenames;
+
+	if (optind < argc)
+		searchpath = argv[optind];
+	optind++;
+	for (; optind < argc; optind++) {
+		filenames.push_back(argv[optind]);
+	}
+
+
+	/* Temporary print statements */
+	std::cout << "searchpath: " << searchpath << std::endl;
+	std::cout << "filename(s): ";
+	for ( auto& filename : filenames) {
+		std::cout << filename << ", ";
+	}
+	std::cout << std::endl;
 }
